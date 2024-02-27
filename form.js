30f1a461e98d84a393462018fcac455ab30649de
MariaEvpalova <mashayevpalova@gmail.com>
2024-02-27 13:16:52 +0300

    Load pages with JS
    - Add rules page
    - Load pages with JS instead of separate HTML files
    - Add form submission (modify later)

---
+++ form.js
@@ -0,0 +1,111 @@
+async function fetchData(pname, params) {
+    let url = `https://sql.lavro.ru/call.php?pname=${pname}&db=312279`;
+
+    params.forEach((param, index) => {
+        url += `&p${index + 1}=${param}`;
+    });
+
+    url += '&format=columns';
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(
+            'There has been a problem with your fetch operation:',
+            error
+        );
+        throw error;
+    }
+}
+
+async function handleFormSubmission(buttonId, pname, loginId, passwordId) {
+    document
+        .getElementById(buttonId)
+        .addEventListener('click', async function (e) {
+            e.preventDefault();
+            const login = document.getElementById(loginId).value;
+            const password = document.getElementById(passwordId).value;
+            try {
+                const data = await fetchData(pname, [login, password]);
+                const firstKey = Object.keys(data['RESULTS'][0])[0];
+                if (firstKey === 'ERROR') {
+                    displayError(data['RESULTS'][0][firstKey]);
+                } else {
+                    displayGamesPage(login);
+                }
+            } catch (error) {
+                console.error(`${pname} failed`, error);
+                displayError(error.message);
+            }
+        });
+}
+
+function displayError(errorMessage) {
+    const errorDiv = document.getElementById('error-message');
+    errorDiv.textContent = errorMessage;
+}
+
+function loadEnter() {
+    document.body.innerHTML = '';
+    document.body.className = '';
+    document.body.classList.add('enter');
+
+    // Define the new HTML content
+    const newHTMLContent = `
+        <a href="index.html">
+            <button class="back">
+                <img src="images/back.svg" alt="back arrow" />
+            </button>
+        </a>
+        <div>
+            <h1>Вход</h1>
+            <form id="loginForm">
+                <div>
+                    <label for="login">Имя пользователя:</label>
+                    <input type="text" id="login" name="login" />
+                </div>
+                <div>
+                    <label for="password">Пароль:</label>
+                    <input type="password" id="password" name="password" />
+                </div>
+                <div>
+                    <button type="button" id="loginButton">Вход</button>
+                </div>
+            </form>
+        </div>
+
+        <div>
+            <h1>Регистрация</h1>
+            <form id="registrationForm">
+                <div>
+                    <label for="reg-login">Имя пользователя:</label>
+                    <input type="text" id="reg-login" name="login" />
+                </div>
+                <div>
+                    <label for="reg-password">Пароль:</label>
+                    <input type="password" id="reg-password" name="password" />
+                </div>
+                <div>
+                    <button type="button" id="registerButton">
+                        Зарегистрироваться
+                    </button>
+                </div>
+            </form>
+        </div>
+    `;
+
+    document.body.innerHTML = newHTMLContent;
+
+    const backButton = document.querySelector('.back');
+    backButton.onclick = function () {
+        loadMain();
+    };
+}
+
+handleFormSubmission('loginButton', 'auth', 'login', 'password');
+handleFormSubmission('registerButton', 'register', 'reg-login', 'reg-password');
---
+++ script.js
@@ -0,0 +1,56 @@
+function loadMain() {
+    document.body.innerHTML = '';
+    document.body.removeAttribute('class');
+    document.body.classList.add('index');
+    const newHTMLContent = `
+          <h1>Концепт</h1>
+          <div class="buttons">
+              <button id="rules">Правила</button>
+              <button id="enter">Играть</button>
+          </div>
+        `;
+
+    document.body.innerHTML = newHTMLContent;
+
+    document.getElementById('rules').onclick = function () {
+        loadRules();
+    };
+    document.getElementById('enter').onclick = function () {
+        loadEnter();
+    };
+}
+
+function loadRules() {
+    document.body.innerHTML = '';
+    document.body.className = 'rules';
+
+    const newHTMLContent = `
+          <a href="index.html">
+              <button class="back">
+                  <img src="images/back.svg" alt="back arrow" />
+              </button>
+          </a>
+          <div>
+              <h1>Правила</h1>
+              <p>
+                  Цель игры — правильно угадывать слова, используя универсальные
+                  иконки и связанные с ними ассоциации. Каждый игрок выбирает
+                  слово (или фразу) и загадывает его другим игрокам. Для этого
+                  ведущий игрок размещает маркеры на определённых иконках игрового
+                  поля. Первый игрок, отгадавший задуманное слово (или фразу),
+                  получает 2 победных очка, а игрок, загадывающий слово получает 1
+                  балл. Тот, кто к концу игры наберёт их больше всех, станет
+                  победителем.
+              </p>
+          </div>
+        `;
+
+    document.body.innerHTML = newHTMLContent;
+
+    const backButton = document.querySelector('.back');
+    backButton.onclick = function () {
+        loadMain();
+    };
+}
+
+loadMain();
